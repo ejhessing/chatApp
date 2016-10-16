941351aa558c6075d5f0ddf5ff405ea43cdcf044
Erwin Hessing <ejhessing@Erwins-MacBook-Pro.local>
2016-10-16 14:17:01 +1300

    Added usernames to document

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const usernames = [];
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,9 +13,34 @@ app.get('/', (req, res) => {
 io.sockets.on('connection', (socket) => {
     console.log('Socket Connected!');
 
+    socket.on('new user', (data, callback) => {
+      if(usernames.indexOf(data) != -1) {
+        callback(false);
+      } else {
+        callback(true);
+        socket.username = data;
+        usernames.push(socket.username);
+        updateUserNames();
+      }
+    })
+
+    function updateUserNames() {
+      io.sockets.emit('usernames', usernames);
+    };
+
     socket.on('send message', (data) => {
-        io.sockets.emit('new message', { msg: data});
+        io.sockets.emit('new message', { msg: data, user:socket.username});
     });
+
+    socket.on('disconnect', (data) => {
+      if(!socket.username) {
+        return;
+      }
+
+      usernames.splice(usernames.indexOf(socket.usernamer), 1);
+      updateUserNames();
+    })
+
 });
 
 
